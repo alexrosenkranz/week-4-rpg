407087cd1fc7bdd280727ba5939be5cfe2c2d75d
Alex Rosenkranz <alexrosenkranz@Alexs-iMac.local>
2016-11-30 15:11:31 -0500

    pseudocode

---
+++ assets/js/game.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+
+  // Code out HTML View for Game
+  	//4 Clickable Character Boxes
+  		// Each has Name, image, and stats
+  	//Space for player character to appear
+  	//Space for enemy to appear
+
+  //Create Variables
+  	// Characters
+  		// array of objects
+  		// each has different properties of health, attack power, counter attack power
+  	// Stats
+
+  // Start new game function
+  	// Sets all characters to be able to be picked by player with reset stats
+  	// Once picked, player character gets put into player area while the rest go into Enemies area
+
+  // 
+
+
+});
