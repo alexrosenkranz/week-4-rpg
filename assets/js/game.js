2614f3f5ad0e89e0cbdc68bbc35a3e8d757e5edc
Alex Rosenkranz <alexrosenkranz@Alexs-iMac.local>
2016-12-02 15:33:30 -0500

    finished game  rules, need to fix newGame function and add styling

--- assets/js/game.js
+++ assets/js/game.js
@@ -11,16 +11,14 @@ $(document).ready(function() {
       // array of objects
       // each has different properties of health, attack power, counter attack power
   var characters = [ ];
-  var playerPicked = true;
-  var enemyPicked = true;
-    
-  	// Active Stats Variables
-  var enemies;
+  var playerPicked;
+  var enemyPicked;
   
   var i = 0;
 
   // Active Characters
   var playerActive;
+  var playerBaseAttack;
   var enemyActive;
 
 
@@ -28,32 +26,72 @@ $(document).ready(function() {
   	// Sets all characters to be able to be picked by player with reset stats
   	// Once picked, player character gets put into player area while the rest go into Enemies area
   function newGame() {
-    characters[0] = {name: 'Kyle Katarn', health: 170, attack: 16, counter: 11, };
-    characters[1] = {name: 'Lando Calrissian', health: 220, attack: 10, counter: 18, };
-    characters[2] = {name: 'IG-88', health: 150, attack: 20, counter: 10, };
-    characters[3] = {name: 'Boba Fett', health: 200, attack: 18, counter: 5,};
-    playerPicked = true;
+    $('.player').empty();
+    $('.enemy').empty();
+    $('.characterList').empty();
+    characters[0] = {
+      name: 'Kyle Katarn', 
+      health: 170, 
+      attack: 16, 
+      counter: 11, 
+      pic: 'assets/images/kylekatarn.jpg' 
+    };
+    characters[1] = {
+      name: 'Lando Calrissian', 
+      health: 220, 
+      attack: 10, 
+      counter: 18, 
+      pic: 'assets/images/lando.jpg' 
+    };
+    characters[2] = {
+      name: 'IG-88', 
+      health: 150, 
+      attack: 20, 
+      counter: 10, 
+      pic: 'assets/images/ig88.jpg' };
+    characters[3] = {
+      name: 'Boba Fett', 
+      health: 200, 
+      attack: 18, 
+      counter: 5, 
+      pic: 'assets/images/bobafett.jpg'
+    };
+    playerPicked = false;
+    enemyPicked = true;
     
+    $(characters).each(function(){
+      $('.characterList').append('<div class="character col-xs-12 col-sm-12 col-md-6"><div class="name"></div><img class="pic"/><div class="stats"></div></div>')
+
+    });
+
     $('.character').each(function() {
       $(this).attr('data-value',i);
       i++;
     });
-
-
     i = 0;
+    
     $('.name').each(function() {
-      $(this).text(characters[i].name);
+      $(this).html(characters[i].name);
       i++;
     });
-
     i = 0
+
+    $('.pic').each(function(){
+      $(this).attr('src', characters[i].pic);
+      i++;
+    });
+    i = 0;
     $('.stats').each(function() {
-      $(this).text('Health: ' + characters[i].health + ' Attack: ' + characters[i].attack + ' Counter: ' + characters[i].counter);
+      $(this).html('Health: ' + characters[i].health + ' Attack: ' + characters[i].attack + ' Counter: ' + characters[i].counter);
       i++;
     });
     i = 0;
     console.log(characters);
+    playerPicked = false;
+    enemyPicked = true;
   }
+
+
 
   newGame();
 
@@ -63,17 +101,85 @@ $(document).ready(function() {
 
 
   $('.character').on('click', function() {
-    if (playerPicked){
+    
+    var p;
+
+    if ((playerPicked === false) && (enemyPicked)) {
       var playerInfo = $(this).html();
-      playerActive = characters.splice(characters[$(this).attr('data-value')], 1);
+      p = $(this).attr('data-value');
+      playerActive = characters[p];
+      playerBaseAttack = playerActive.attack;
+      characters.splice(p, 1);      
       console.log(playerActive);
-      $(this).css('display', 'none');
+      console.log(characters);
+      $(this).remove();
       $('.player').html(playerInfo);
-    };
-    playerPicked = false;
+      $('.character').each(function() {
+        $(this).attr('data-value',i);
+        i++;
+      });
+
+      $('.player .stats').html('Health: ' + playerActive.health + ' Attack: ' + playerActive.attack + ' Counter: ' + playerActive.counter);
+      i = 0;
+      playerPicked = true;
+      enemyPicked = false;
+    }
+
+    else if ((enemyPicked === false) && (playerPicked === true)) {
+      var enemyInfo = $(this).html();
+      p = $(this).attr('data-value');
+      enemyActive = characters[p];
+      characters.splice(p,1);
+      console.log(enemyActive);
+      console.log(characters);
+      $(this).remove();
+      $('.enemy').html(enemyInfo);
+      $('.character').each(function() {
+        $(this).attr('data-value',i);
+        i++;
+      });
+      $('.enemy .stats').html('Health: ' + enemyActive.health + ' Attack: ' + enemyActive.attack + ' Counter: ' + enemyActive.counter);
+
+      i = 0;
+      enemyPicked = true;
+    }
+    else if (enemyPicked && playerPicked) {
+      return;
+    }
   });
 
+
+  // playerActive & enemyActive
+
     // Player attacks enemy
+
+  $('.attack').on('click', function() {
+    if (enemyActive.health <= 0) {
+      $('.enemy').html('<h2>Enemy vanquished, pick a new battle</h2>');
+      playerActive.attack = playerBaseAttack;
+      enemyPicked = false;
+    }
+
+    else if (enemyActive.health > 0 && playerActive.health > 0){
+      enemyActive.health = enemyActive.health - playerActive.attack;
+      $('.enemy .stats').html('Health: ' + enemyActive.health + ' Attack: ' + enemyActive.attack + ' Counter: ' + enemyActive.counter);
+      playerActive.attack = playerActive.attack + playerBaseAttack;
+
+      playerActive.health = playerActive.health - enemyActive.counter;
+      $('.player .stats').html('Health: ' + playerActive.health + ' Attack: ' + playerActive.attack + ' Counter: ' + playerActive.counter);
+
+    }
+
+    else if (playerActive.health <= 0) {
+      $('.player').html('<h2>You lose! Start a new game. </h2>');
+    }
+
+  });
+
+
+  $('.newGame').on('click', function() {
+    newGame();
+  });
      // Enemy loses health (health - attack)
       // Enemy counterattacks
       //  Increase player attack stat by base attack power
